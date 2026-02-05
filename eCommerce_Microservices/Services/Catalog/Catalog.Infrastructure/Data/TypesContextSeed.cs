e32edf4024e76184cd1165f44bb8bba5dda2bd0e
2k4CSE124 <31066663+2k4CSE124@users.noreply.github.com>
2026-02-05 14:19:45 +0500

    SeedData
    Created Seed Files
    Created ContextSeed files
    set Seed files property "Copy Always"

---
+++ eCommerce_Microservices/Services/Catalog/Catalog.Infrastructure/Data/TypesContextSeed.cs
@@ -0,0 +1,29 @@
+﻿using Catalog.Core.Entities;
+using MongoDB.Driver;
+using System.Text.Json;
+
+namespace Catalog.Infrastructure.Data
+{
+    public static class TypesContextSeed
+    {
+        public static void SeedData(IMongoCollection<ProductType> typeCollection)
+        {
+            bool checkTypes = typeCollection.Find(b => true).Any();
+            if (!checkTypes)
+            {
+                string path = Path.Combine("Data", "SeedData", "types.json");
+
+                var typesData = File.ReadAllText(path);
+                var productTypes = JsonSerializer.Deserialize<List<ProductType>>(typesData);
+                if (productTypes != null)
+                {
+                    foreach (var item in productTypes)
+                    {
+                        typeCollection.InsertOneAsync(item);
+                    }
+                }
+            }
+        }
+    }
+
+}
